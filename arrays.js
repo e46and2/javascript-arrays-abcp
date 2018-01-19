8548556ae08594f9effad3e1bf051a3f7cd7794a
Emily <e.nicholl.liepe@gmail.com>
2018-01-19 17:08:10 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,23 @@
+var ingredient1 = "bread"
+var ingredient2 = "mild cheese"
+var ingredient3 = "sharp cheese"
+var ingredient4 = "butter"
+var ingredient5 = "tomato"
+var ingredient6 = "garlic"
+
+//An example of array literals is listed below
+//var myArray = [element0, element1, ..., elementN];
+
+var primeNumbers = [2,3,5,7,11,13,17,19,23,29,31,37];
+
+var tvShows = ["game of thrones", "true detective", "the good wife", "empire"];
+
+var weirdGreeting = [ "he", 110, "w", 0, "r", 1, "d" ];
+
+var emtpy = [];
+
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+addElementToBeginningOfArray()
+
+destructivelyAddElementToBeginningOfArray
